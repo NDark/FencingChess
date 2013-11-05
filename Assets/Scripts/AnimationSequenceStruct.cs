0c389e658fe2086d4ead7cb216fcd5c315e918bc
NDark <ndarkteng@gmail.com>
2013-11-05 16:22:57 +0800

    add LevelGenerator.cs

---
+++ Assets/Scripts/AnimationSequenceStruct.cs
@@ -0,0 +1,13 @@
+﻿/*
+@file AnimationSequenceStruct.cs
+@author NDark
+@date 20131105 file started.
+*/
+using UnityEngine;
+using System.Collections.Generic ;
+
+public class AnimationSequenceStruct 
+{
+	public string m_AnimationTag = "" ;
+	public List<string> m_ImageFilepath = new List<string>() ;
+}
